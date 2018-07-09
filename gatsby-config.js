13233b4fc31870e96ff7f7e0fc0e263e4cf25360
Rachael Bermudes <bermudesorma@gmail.com>
2018-07-09 15:52:26 -0700

    add contentful cli and js files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'My Contentful Blog',
+    title: 'Contentful Blog',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId:'j8rtz7kjb9ce',
+      accessToken: '90db1f2ce256dbabf3d8f426d19107a8ef027ebf4bb4550508f437fcde591025'
+    }
+  },
+    'gatsby-plugin-react-helmet'
+  ],
 }
+
+
